8cb4dc199cf1d8152b69ba2e6562c662683aab54
Jack Pena <penajackc@gmail.com>
2022-03-14 18:16:56 -0700

    commit tree a674f3db6cfa24593678681da0c5f40b4366fe4a

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,27 @@
 class Post < ApplicationRecord
+    
+
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: { in: %w(Fiction Non-Fiction)}
+
+    validate :clickbait?
+
+
+    def clickbait?
+        clickbait = [/Won't Believe/i, /Secret/i, /Top \d/i, /Guess/i]
+        if clickbait.none? {|pattern| pattern.match title }
+            errors.add(:title, "Title is not clickbait")
+        end
+    end
+    
+    # title inaccessible here
+    # def clickbait?
+    #     clickbait = ["Won't Believe", "Secret", "Top", "Guess"]
+    #     if clickbait.none? {|stg| title.include?(stg) }
+    #         errors.add(:title, "Title is not clickbait")
+    #     end
+    # end
+    
 end
